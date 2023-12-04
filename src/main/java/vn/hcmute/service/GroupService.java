52220592575393c828e7adc1a5147b0708bb6f43
Lemon <ledinhdattin92017@gmail.com>
2023-12-04 12:11:46 +0700

    functional show post, like, join group successfully

---
+++ src/main/java/vn/hcmute/service/GroupService.java
@@ -0,0 +1,33 @@
+package vn.hcmute.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import vn.hcmute.Responsitory.GroupRepository;
+import vn.hcmute.entities.GroupEntity;
+
+
+
+@Service
+public class GroupService implements IGroupService{
+	@Autowired
+	GroupRepository groupRepo;
+
+	@Override
+	public List<GroupEntity> findAll() {
+		return groupRepo.findAll();
+	}
+
+	@Override
+	public Optional<GroupEntity> findById(Long id) {
+		return groupRepo.findById(id);
+	}
+	
+	
+	
+	
+
+}
