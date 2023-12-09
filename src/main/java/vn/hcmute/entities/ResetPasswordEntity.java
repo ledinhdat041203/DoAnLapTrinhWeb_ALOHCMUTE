79f1c69acb556e68b2f269b69069be7e0f5a86a9
TuanDat01 <tuandatt2003@gmail.com>
2023-12-09 12:35:11 +0700

    updateFix

--- src/main/java/vn/hcmute/entities/ResetPasswordEntity.java
+++ src/main/java/vn/hcmute/entities/ResetPasswordEntity.java
@@ -1,22 +1,11 @@
 package vn.hcmute.entities;
 
 import java.io.Serializable;
+import java.util.Calendar;
 import java.util.Date;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+import jakarta.persistence.*;
+
 @Entity
 public class ResetPasswordEntity implements Serializable{
 	private static final long serialVersionUID = 1L;
@@ -26,16 +15,53 @@ public class ResetPasswordEntity implements Serializable{
 	@Column(name = "id_pass", columnDefinition = "BIGINT")
 	private Long id;
 	private String token;
-	
 	@OneToOne(targetEntity = UserEntity.class,fetch = FetchType.EAGER)
 	@JoinColumn(name = "id_pass_account", referencedColumnName = "idaccount")
 	private UserEntity userResetPass;
-	
 	private Date expireDate;
-	
 	public ResetPasswordEntity(String token, UserEntity userResetPass) {
 		this.token = token;
 		this.userResetPass = userResetPass;
 	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getToken() {
+		return token;
+	}
+	public void setToken(String token) {
+		this.token = token;
+	}
+	public UserEntity getUserResetPass() {
+		return userResetPass;
+	}
+	public void setUserResetPass(UserEntity userResetPass) {
+		this.userResetPass = userResetPass;
+	}
+	
+	public Date getExpireDate() {
+		return expireDate;
+	}
+	@PrePersist
+    private void setExpiryDate() {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(new Date());
+        cal.add(Calendar.MINUTE, EXPIRATION);
+        this.expireDate = cal.getTime();
+    }
+	public ResetPasswordEntity() {
+	}
+
+	
+	
+	
+	
+	
+	
+
+	
 
 }
